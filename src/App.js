94f1fb48408b99923c573b5083f1aab20286930b
unknown <itriparm@gmail.com>
2024-03-03 08:13:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import WebApp from '@twa-dev/sdk';
+import { TonConnectButton, useTonWallet } from '@tonconnect/ui-react';
 
 function App() {
+  const wallet = useTonWallet();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{backgroundColor: WebApp.headerColor}}>
+        <div className='Profile-Picture' style={{backgroundImage: 'url("https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/08/bored_ape_yacht_club.jpeg-1.jpg")'}}></div>
+        <TonConnectButton />
       </header>
+      {wallet && 
+            <div>
+                <span style={{color:'red'}}>Connected wallet: {wallet.name}</span>
+                <span style={{color:'red'}}>Device: {wallet.device.appName}</span>
+            </div>
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WebApp from '@twa-dev/sdk'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+WebApp.ready();
+WebApp.enableClosingConfirmation()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TonConnectUIProvider 
+  actionsConfiguration={{
+    twaReturnUrl: 'https://t.me/BazzarBeloteBot'
+  }} manifestUrl="https://3a34-2a02-2a57-f7eb-0-c1b3-f690-9b01-78a.ngrok-free.app/tonconnect-manifest.json">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TonConnectUIProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
